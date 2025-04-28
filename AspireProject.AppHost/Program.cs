8fba6be1c2dded91daf145a7cd216e121840caa6
patrick <patrick.michaels1337@gmail.com>
2025-04-28 22:22:05 +0100

    Some intitial setup

--- AspireProject.AppHost/Program.cs
+++ AspireProject.AppHost/Program.cs
@@ -1,5 +1,30 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.API>("api");
+var sql = builder.AddSqlServer("sql")
+    //.WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("dbVolume");
+
+var serviceBus = builder.AddAzureServiceBus("Queue")
+    .RunAsEmulator();
+
+var storage = builder.AddAzureStorage("storage")
+    .RunAsEmulator();
+
+var events = builder.AddAzureEventHubs("events")
+    .RunAsEmulator();
+
+var db = sql.AddDatabase("database");
+
+var api = builder.AddProject<Projects.API>("api")
+    .WithReference(db);
+
+builder.AddNpmApp("reactvite", "../frontend")
+    .WithReference(api)
+    .WithEnvironment("BROWSER", "none")
+    .WithHttpEndpoint(env: "VITE_PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.AddProject<Projects.Functions>("functions");
 
 builder.Build().Run();
